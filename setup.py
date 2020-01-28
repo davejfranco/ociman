f26c00f567a15e35152e9363d65ea6df9ac63e31
djfranco <djfranco@DJFRANCO-CL.cl.oracle.com>
2020-01-28 10:49:06 -0300

    ociman LICENSE

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name = 'ociman',
-    description='Commandline tool to interact with oci',
+    description='Commandline tool to automate oci ops',
     version = '0.1.0',
     author='Dave Franco',
     author_email='davefranco1987@gmail.com',
-    license='MIT',
+    license='Apache License 2.0',
     packages = ['ociman'],
     entry_points = {
         'console_scripts': [
