89b1f19d290e3a6cc87496ce3eb5ac706a7eb30b
Dave Franco <davefranco1987@gmail.com>
2020-01-28 14:41:05 -0300

    tag validation

--- ociman/__main__.py
+++ ociman/__main__.py
@@ -26,14 +26,14 @@ def main():
         keyvalue = arg.tag.split(":")
         instances = cli.list_vms_by_tag(keyvalue[0],keyvalue[1])
         cli.manage_instance(arg.action.upper(), instances)
+    
+    if arg.list and arg.tag:
+        if not tag_validator(arg.tag):
+            print("Not a valid tag")
+            sys.exit(0)
 
 
 
-
-    # #Create Instance
-    # if arg.create_instance and arg.image_name is not None:
-    #     db.create_instance(arg.image_name, arg.env, arg.own)
-    #     sys.exit(0)
 
 
 if __name__ == "__main__":
